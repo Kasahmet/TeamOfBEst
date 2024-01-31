92d90144ebb09945f4c08ff9edf4d0f996f05194
tohakim29 <tohakim29@gmail.com>
2024-01-31 12:56:02 -0500

    week01 method in task added

--- src/takhir/week01/DivideWithoutOperator.java
+++ src/takhir/week01/DivideWithoutOperator.java
@@ -2,6 +2,18 @@ package takhir.week01;
 
 public class DivideWithoutOperator {
     public static void main(String[] args) {
+      methodName(50,9);
+    }
+
+    public static void methodName(int firstNum , int secondNum){
 
     }
 }
+
+
+/*
+*
+* Write a method that can divide
+*  two numbers without using division operator.
+*
+* */
