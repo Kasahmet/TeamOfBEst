a9de20c5192cd741c27de25cea4b934c931c7d2c
Seda <sedakant@gmail.com>
2024-01-31 13:34:56 -0500

    added week1

---
+++ src/seda/week01/Task2_FINRAResults.java
@@ -0,0 +1,28 @@
+package seda.week01;
+
+public class Task2_FINRAResults {
+
+    public static void printFinraNumbers() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printFinraNumbers();
+    }
+}
+/*
+Write a method that prints out the numbers from 1 to 30 but;
+for numbers which are multiple of 3 print "FIN" instead of the number,
+for numbers which are multiple of 5, print "RA" instead of the number,
+and for numbers which are multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
