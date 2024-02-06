a96d5d44db9b503e80af52f0858f7862fc0b0de9
sule <inanirsule@gmail.com>
2024-02-06 16:36:47 -0500

    week 2 solution divisible

---
+++ src/sule/week2/Divisible.java
@@ -0,0 +1,18 @@
+package sule.week2;
+
+public class Divisible {
+    public class Main {
+        public static void main(String[] args) {
+
+            for (int i = 1; i <= 100; i++) {
+                if (i % 15 == 0) {
+                    System.out.println(i + " - Divisible by 15");
+                } else if (i % 3 == 0) {
+                    System.out.println(i + " - Divisible by 3");
+                } else if (i % 5 == 0) {
+                    System.out.println(i + " - Divisible by 5");
+                }
+            }
+        }
+    }
+}
