8cf8badca94518de39f2f93630daebec21aa21fd
ajbmb <jasonbowling670@gmail.com>
2024-02-07 10:54:53 -0500

    solved divisible by 3,5 or 15 task

---
+++ src/Jay/week02/DivisibleBy3_5_15_Jay.java
@@ -0,0 +1,25 @@
+package Jay.week02;
+
+public class DivisibleBy3_5_15_Jay {
+
+    public static void main(String[] args) {
+
+        String by3 = "";
+        String by5 = "";
+        String by15 = "";
+
+        for (int i = 1; i <= 100; i++) {
+            if( i % 15 == 0 ){
+                by15 += i + " ";
+            } else if (i % 5 == 0) {
+                by5 += i + " ";
+            } else if (i % 3 ==0) {
+                by3 += i + " ";
+            }
+        }
+
+        System.out.println("Divisible by 3: " + by3);
+        System.out.println("Divisible by 5: " + by5);
+        System.out.println("Divisible by 15: " + by15);
+    }
+}
