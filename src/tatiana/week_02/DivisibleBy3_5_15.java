cccccb48156c14b31937efac53b12186e4ed593b
Tatiana Osipova <tatigerbstnyc@gmail.com>
2024-02-21 10:41:46 -0500

    Updated task DivisibleBy3_5_15.java

--- src/tatiana/week_02/DivisibleBy3_5_15.java
+++ src/tatiana/week_02/DivisibleBy3_5_15.java
@@ -2,29 +2,30 @@ package tatiana.week_02;
 
 public class DivisibleBy3_5_15 {
     public static void main(String[] args) {
-        //I will create separate Strings for all three category, to store divisible numbers
-        String divisibleBy3 = "";
-        String divisibleBy5 = "";
-        String divisibleBy15 = "";
+        // Creating StringBuilder objects for each category
+        StringBuilder divisibleBy3 = new StringBuilder();
+        StringBuilder divisibleBy5 = new StringBuilder();
+        StringBuilder divisibleBy15 = new StringBuilder();
 
-        // I will use loop to go through 1 to 100
+        // Looping through numbers 1 to 100
         for (int i = 1; i <= 100; i++) {
-            //1 - if the number can be divisible by 3,5 and 15, then it should only be displayed in DivisibleB15 section
-            if (i % 15 == 0) { // will start with divisibleBy15, as those numbers are divisible by 3,5 and 15
-                //if i divisible by 15 with no reminder
-                divisibleBy15 += i + " ";  //adding empty space to make numbers readable, when I print
-
-                //2 - if the number can be divisible by 3 but can't be divisible by 15 then it should only be displayed in DivisibleB15 section
+            if (i % 15 == 0) {
+                divisibleBy15.append(i).append(" "); // Appending to StringBuilder
             } else if (i % 3 == 0) {
-                divisibleBy3 += i + " ";
-
-                //3 - if the number can be divisible by 5 but can't be divisible by 15 then it should only be displayed in DivisibleB15 section
+                divisibleBy3.append(i).append(" ");
             } else if (i % 5 == 0) {
-                divisibleBy5 += i + " ";
+                divisibleBy5.append(i).append(" ");
             }
         }
-        System.out.println("Divisible By 15: " + divisibleBy15);
-        System.out.println("Divisible By 3: " + divisibleBy3);
-        System.out.println("Divisible By 5: " + divisibleBy5);
+
+        // Converting StringBuilder objects to strings for output
+        String resultBy15 = divisibleBy15.toString();
+        String resultBy3 = divisibleBy3.toString();
+        String resultBy5 = divisibleBy5.toString();
+
+        // Printing the results
+        System.out.println("Divisible By 15: " + resultBy15);
+        System.out.println("Divisible By 3: " + resultBy3);
+        System.out.println("Divisible By 5: " + resultBy5);
     }
 }
