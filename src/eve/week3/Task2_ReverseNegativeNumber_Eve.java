6cce36c94e8d8fc67fa8772d14b3ead1dfadba9a
Eve <eve.steel@cydeo.com>
2024-02-21 20:54:35 +0300

    week 3 - solution of the Task 2 was created

--- src/eve/week3/Task2_ReverseNegativeNumber_Eve.java
+++ src/eve/week3/Task2_ReverseNegativeNumber_Eve.java
@@ -1,4 +1,16 @@
 package eve.week3;
 
 public class Task2_ReverseNegativeNumber_Eve {
+
+    public static int reverseNum(int  num) {
+        String str = new StringBuilder(""+num).reverse().toString();
+        if(num < 0) {
+            str = "-"+str.substring(0, str.length()-1);
+        }
+        return Integer.valueOf(str);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseNum(-10));
+    }
 }
