e595bbbbf6dbbdd58b67089706dad37234e24746
Eve <eve.steel@cydeo.com>
2024-02-21 20:13:01 +0300

    week2 task1 solution was added

--- src/eve/week2/Task1_SwapNumbers_Eve.java
+++ src/eve/week2/Task1_SwapNumbers_Eve.java
@@ -1,8 +1,28 @@
 package eve.week2;
 
 public class Task1_SwapNumbers_Eve {
-    public static void main(String[] args) {
+    //Solution 1:
 
+
+    public static void swaptwonumbers1(int a, int b){
+
+
+        a = a +b;
+        b = a - b;
+        a = a - b;
     }
+
+
+
+    //Solution 2:
+    public static void swaptwonumbers2(int a, int b){
+
+//here a and b should not be 0
+
+        a = a*b;
+        b = a/b;
+        a = a/b;
+    }
+
 
 }
