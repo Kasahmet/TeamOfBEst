315b6653fa8a68796f85890d34ec277d1610b67e
Eve <eve.steel@cydeo.com>
2024-02-21 20:10:11 +0300

    week1 task solutions were added

---
+++ src/eve/week1/Task3_FINRA.java
@@ -0,0 +1,31 @@
+package eve.week1;
+
+public class Task3_FINRA {
+/*
+Write a method which prints out the numbers from 1 to 30
+but for numbers which are a multiple of 3, print "FIN" instead of the number
+and for numbers which are a multiple of 5, print "RA" instead of the number.
+for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+
+ */
+public static void FINRA() {
+    String result = "";
+    for(int i=1; i <= 30; i++) {
+        if(i % 5==0 && i %3 ==0)
+            result += "FINRA ";
+        else if(i%5 == 0)
+            result += "RA ";
+        else if(i%3==0)
+            result+="FIN ";
+        else
+            result += i+" ";
+    }
+    System.out.println(result);
+}
+
+    public static void main(String[] args) {
+      FINRA();
+    }
+
+
+}
