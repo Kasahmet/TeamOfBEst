c60fcd851000962239bcf06ffb0eb984df8eb6ef
Eve <eve.steel@cydeo.com>
2024-02-28 11:51:14 +0300

    Week 4 Task 1 solution was created

---
+++ src/eve/week4/Task2_SameLetters_Eve.java
@@ -0,0 +1,76 @@
+package eve.week4;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class Task2_SameLetters_Eve {
+    /*
+    Write a  function that check if a string is build out of the same letters as another string.
+            Ex: same("abc", "cab"); -> true , Solution 1:
+                same("abc", "abb"); -> false:
+
+     */
+
+    // solution 1:
+    public boolean same(String a, String b) {
+        char[] ch1 = a.toCharArray();
+        char[] ch2 = b.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        String a1 = "", a2 = "";
+
+        for (char each : ch1) {
+            a1 += each;
+        }
+
+        for (char each : ch2) {
+            a2 += each;
+        }
+
+        return a1.equals(a2);
+    }
+
+
+    // solution 2
+    public boolean Same(String a, String b) {
+        a = new TreeSet<String>(Arrays.asList(a.split(""))).toString();
+        b = new TreeSet<String>(Arrays.asList(b.split(""))).toString();
+        return a.equals(b);
+    }
+
+    //solution 3
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert the strings to lowercase for case-insensitive comparison
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Check if the lengths of the strings are equal
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert both strings to character arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare the sorted character arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "cab";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
+
+
