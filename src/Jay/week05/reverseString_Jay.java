b12df0449afe67d4d4929cfd38b3f239744cf932
ajbmb <jasonbowling670@gmail.com>
2024-03-06 15:14:00 -0500

    completed tasks

---
+++ src/Jay/week05/reverseString_Jay.java
@@ -0,0 +1,20 @@
+package Jay.week05;
+
+public class reverseString_Jay {
+
+    public static void main(String[] args) {
+
+        String str = "abc";
+        String str2 = "";
+
+        for (int i = str.length()-1; i >= 0; i--) {
+
+            System.out.print(str.charAt(i));
+
+        }
+    }
+}
+/*
+Write a function that can reverse a String
+Ex: Reverse("ABCD"); ==> DCBA *
+ */
---
+++ src/Jay/week05/uniqueChars_Jay.java
@@ -0,0 +1,34 @@
+package Jay.week05;
+
+public class uniqueChars_Jay {
+
+    public static void main(String[] args) {
+
+        System.out.println(returnUniqueChars("aabbcc"));
+        System.out.println(returnUniqueChars("abcc"));
+    }
+
+    public static String returnUniqueChars(String str){
+
+        String finalString = ""; // aybba
+        int count;
+
+        for (int i = 0; i < str.length(); i++) {
+            count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)){
+                    count ++;
+                }
+            }
+            if(count == 1){
+                finalString += str.charAt(i);
+            }
+        }
+
+        return finalString;
+}}
+/*
+ String - Find the unique
+Write a return method that can find the unique characters from the String
+ Ex: unique("AAABBBCCCDEF") ==> "DEF";
+ */
