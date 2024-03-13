1912f62f585a66f070b0b0561ff18ffe6d74c952
ciearra <riendeau.ck@gmail.com>
2024-03-13 12:41:26 -0400

    Week 01 tasks

---
+++ src/ciearra/Week01/FinRa.java
@@ -0,0 +1,21 @@
+package ciearra.Week01;
+
+public class FinRa {
+    public static void main(String[] args) {
+        FINRA();
+    }
+    public static void FINRA() {
+        for(int i = 1; i <= 30; i++){
+            if (i % 15 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
