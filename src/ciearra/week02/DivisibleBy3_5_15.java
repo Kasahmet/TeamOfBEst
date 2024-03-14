b7fc70df01ae5d403a8fd1488fda3cc7a7bddba5
ciearra <riendeau.ck@gmail.com>
2024-03-13 21:57:03 -0400

    updated test

--- src/ciearra/week02/DivisibleBy3_5_15.java
+++ src/ciearra/week02/DivisibleBy3_5_15.java
@@ -8,16 +8,16 @@ public class DivisibleBy3_5_15 {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
                 divisibleBy15 += i + " ";
-                } else if (i % 5 == 0) {
-                    divisibleBy5 += i + " ";
-                } else if (i % 3 == 0) {
-                    divisibleBy3 += i + " ";
-                }
+            } else if (i % 5 == 0) {
+                divisibleBy5 += i + " ";
+            } else if (i % 3 == 0) {
+                divisibleBy3 += i + " ";
             }
-            System.out.println("Divisible By 3: " + divisibleBy3);
-            System.out.println("Divisible By 5: " + divisibleBy5);
-            System.out.println("Divisible By 15: " + divisibleBy15);
         }
-
+        System.out.println("Divisible By 3: " + divisibleBy3);
+        System.out.println("Divisible By 5: " + divisibleBy5);
+        System.out.println("Divisible By 15: " + divisibleBy15);
     }
+
+}
 
