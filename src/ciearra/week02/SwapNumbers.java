97ae8df4958a5692f195d853b116f2a3eca41582
ciearra <riendeau.ck@gmail.com>
2024-03-13 22:14:18 -0400

    Week02 Swapnumbers task

--- src/ciearra/week02/SwapNumbers.java
+++ src/ciearra/week02/SwapNumbers.java
@@ -3,7 +3,7 @@ package ciearra.week02;
 public class SwapNumbers {
     public static void main(String[] args) {
         int num1 = 400;
-        int num2 = 30;
+        int num2 = 3;
 
         System.out.println("Original numbers: num1 = " + num1 + " & num2 = " + num2);
         num1 = num1 + num2;
