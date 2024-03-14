bd29341478946d3c4afd4b361c285bc7908ea2fc
ciearra <riendeau.ck@gmail.com>
2024-03-13 22:13:10 -0400

    Week02 Swapnumbers task

---
+++ src/ciearra/week02/SwapNumbers.java
@@ -0,0 +1,18 @@
+package ciearra.week02;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int num1 = 400;
+        int num2 = 30;
+
+        System.out.println("Original numbers: num1 = " + num1 + " & num2 = " + num2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+
+        System.out.println("After swapping the numbers the results are as follows: num1 = " + num1 + " & num2 = " + num2);
+
+
+    }
+}
